bf76ba25739a4cdb62aa674221bfa46a7a107316
Bruno Alves <bruno6821@gmail.com>
2022-11-06 10:48:58 -0300

    Organizando os Imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import './App.css';
-import StartScreen from './components/StartScreen';
+// Imports do REACT
+import { useCallback, useEffect, useState } from "react";
+
+// Imports de CSS
+import "./App.css";
+
+// Imports de DADOS
+import { wordsList } from "./data/words";
+
+// Imports de COMPONENTES
+import StartScreen from "./components/StartScreen";
 
 function App() {
   return (
