6362e2a3e4998e9ed1961139dcb4f3ef7786883d
Bruno Alves <bruno6821@gmail.com>
2022-11-06 10:29:56 -0300

    Lógica para Pegar a Categoria e a Palavra Secreta.

--- src/App.js
+++ src/App.js
@@ -21,10 +21,39 @@ const stages = [
 function App() {
   // Inicia o app com o estágio na posição 0 ou "start"
   const [gameStage, setGameStage] = useState(stages[0].name);
+  //
   const [words] = useState(wordsList);
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickedWordAndCategory = () => {
+    // Captura as chaves do Objeto Words, que são as Categorias
+    const categories = Object.keys(words);
+    // Captura uma Categoria aleatória
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    // Captura uma palavra aleatória dentro da Categoria selecionanda
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    return [word, category];
+  };
 
   // Função para iniciar o jogo, mudando o stage para "game"
   const startGame = () => {
+    // Chama a função para pegar a categoria e a palavra, no Inicio do game
+    const { word, category } = pickedWordAndCategory();
+
+    // Cria um Array com as Letras da Palavra selecionada
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((lettter) => lettter.toLowerCase());
+
+    // Preenchendo os Estados
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   };
 
