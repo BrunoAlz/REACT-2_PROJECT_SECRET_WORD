edf2f97a081ec5aae8d48d27235af54032ca41d6
Bruno Alves <bruno6821@gmail.com>
2022-11-06 10:43:39 -0300

    Adicionando CSS Button, ajustes CC Startscreen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import StartScreen from './components/StartScreen';
 
 function App() {
   return (
     <div className="App">
       <h1>Palavra Secreta</h1>
+      <StartScreen />
     </div>
   );
 }
---
+++ src/components/StartScreen.js
@@ -0,0 +1,12 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <p>Clique no Botão para iniciar o Jogo</p>
+      <button>COMEÇAR O JOGO</button>
+    </div>
+  );
+};
+
+export default StartScreen;
