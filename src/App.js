d52cd04157f075356ad63bc299cdcadcfbefe6a3
Bruno Alves <bruno6821@gmail.com>
2022-11-06 10:53:56 -0300

    Criação dos Estágios do Jogo.

--- src/App.js
+++ src/App.js
@@ -10,11 +10,24 @@ import { wordsList } from "./data/words";
 // Imports de COMPONENTES
 import StartScreen from "./components/StartScreen";
 
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 function App() {
+
+  // Inicia o app com o estágio na posição 0 ou "start"
+  const [gameStage, steGameStage] = useState(stages[0].name);
+
   return (
     <div className="App">
       <h1>Palavra Secreta</h1>
-      <StartScreen />
+      {/* verifica o Estágio do jogo e exibe o componente necessário*/}
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <StartScreen />}
+      {gameStage === 'end' && <StartScreen />}
     </div>
   );
 }
